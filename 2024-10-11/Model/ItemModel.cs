f69d25e2ef204c8cf6b2182f4609971f0e39126d
Warrior Ma <potatofish99@163.com>
2024-10-18 18:13:04 +0800

    完善第一个页面

---
+++ 2024-10-11/Model/ItemModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_10_11.Model
+{
+    class ItemModel
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public bool IsChecked { get; set; }
+
+        public ItemModel(string name, string url, bool isChecked)
+        {
+            Name = name;
+            Url = url;
+            IsChecked = isChecked;
+        }
+
+        public ItemModel()
+        {
+        }
+
+    }
+}
--- 2024-10-11/Views/MainWindow.xaml.cs
+++ 2024-10-11/Views/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using _2024_10_11.Model;
 
 namespace _2024_10_11
 {
@@ -19,17 +20,25 @@ namespace _2024_10_11
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+            List<ItemModel> items = new List<ItemModel>
         {
+            new ItemModel { Name = "Teams", Url = "pack://application:,,,/Resources/1.png", IsChecked = true },
+            new ItemModel { Name = "Lenovo Display Control Center", Url = "pack://application:,,,/Resources/2.png", IsChecked = false },
+            new ItemModel { Name = "Lenovo Accessories and Display Manager", Url = "pack://application:,,,/Resources/3.png", IsChecked = false },
+            new ItemModel { Name = "联想智会软件", Url = "pack://application:,,,/Resources/4.jpg", IsChecked = false }
+        };
+
+            this.DataContext = items;
+
         }
 
-        private void Button_Click2(object sender, RoutedEventArgs e)
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
+            // 关闭当前窗口
+            this.Close();
         }
-
-
 
     }
 }
